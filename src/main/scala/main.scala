379e9ae2d8f355e1180e266d0a7ff02b121d5257
Udara Weerasinghe <udaraweerasinghe968@gmail.com>
2020-05-19 16:26:17 +0530

    write volumeOfSphere function

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,17 @@
+import scala.math.{Pi, pow}
 object main {
   def main(args: Array[String]) = {
     println(temperature())
+    println(volumeOfSphere())
   }
   def temperature() : Double = {
     val celsius = 35
     val fahrenheit = celsius * 1.8 + 32
     fahrenheit
+  }
+  def volumeOfSphere() : Double = {
+    val radius = 5
+    val volume = (4 / 3) * Pi * pow(radius, 3)
+    volume
   }
 }
