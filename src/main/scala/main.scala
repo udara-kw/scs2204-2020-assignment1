e100e7f6cc11df6fe463a9707d5a59f15ba377ba
Udara Weerasinghe <udaraweerasinghe968@gmail.com>
2020-05-19 16:57:03 +0530

    write wholesaleCost function use parameters in functions

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,17 +1,25 @@
 import scala.math.{Pi, pow}
+
 object main {
   def main(args: Array[String]) = {
-    println(temperature())
-    println(volumeOfSphere())
+    println(temperature(celsius = 35))
+    println(volumeOfSphere(radius = 5))
+    println(wholesaleCost(amount = 60))
   }
-  def temperature() : Double = {
-    val celsius = 35
-    val fahrenheit = celsius * 1.8 + 32
-    fahrenheit
+
+  def temperature(celsius:Double) : Double = {
+    celsius * 1.8 + 32
   }
-  def volumeOfSphere() : Double = {
-    val radius = 5
-    val volume = (4 / 3) * Pi * pow(radius, 3)
-    volume
+
+  def volumeOfSphere(radius:Double) : Double = {
+    (4 / 3) * Pi * pow(if (radius>0) radius else 0, 3)
+  }
+
+  def wholesaleCost(amount:Int) : Double = {
+    val coverPrice : Double = 24.95
+    val discount : Double = coverPrice * (40/100)
+    val shippingCost : Double = if (amount<=50) 3 else 3 + (amount-50)*0.75 //considered shipping cost for whole first 50 books as Rs.3
+    val wholesaleCost : Double = shippingCost + ((coverPrice - discount) * amount)
+    wholesaleCost
   }
 }
