565abcefefd55aa73315619794b0df476e5710e2
Udara Weerasinghe <udaraweerasinghe968@gmail.com>
2020-05-19 16:17:02 +0530

    write temperature function

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,3 +1,10 @@
-class main {
-
+object main {
+  def main(args: Array[String]) = {
+    println(temperature())
+  }
+  def temperature() : Double = {
+    val celsius = 35
+    val fahrenheit = celsius * 1.8 + 32
+    fahrenheit
+  }
 }
